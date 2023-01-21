26ebc98d412a9406d4f451675026f5daf2b98ecf
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-01-21 07:45:48 +0100

    routing fait avec navbar partout sauf sur l'accueil

--- ToDo.md
+++ ToDo.md
@@ -1,6 +1,9 @@
 2023.01.20 : 
 
 Faire le routing
+
 Sur l'accueil, des images/div servent de lien
+
 Sur les autres pages, la partie gauche (cf cv from scratch) servira de menu
 sous les width : 768px, un menu circulaire se déploira sur un layer de fond
+Ce menu contient un lien qui ramène à l'accueil
--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./Style/index.css";
-import {BrowserRouter, Router, Routes} from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom";
 import Accueil from "./Pages/Accueil";
+import CV from "./Pages/CV";
+import Informatique from "./Pages/Informatique";
+import Partages from "./Pages/Partages";
+import Parcours from "./Pages/Parcours";
+import Portfolio from "./Pages/Portfolio";
+import NotFound from "./Pages/NotFound";
+import Navbar from "./component/Navbar";
 
 function App() {
+
+  const location = useLocation()
+  console.log(location)
+
   return (
-    <div className="App">
-      <Accueil/>
-    </div>
+    <>
+      {location.pathname!=="/" ? <Navbar /> : ""}
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/informatique" element={<Informatique />} />
+          <Route path="/partages" element={<Partages />} />
+          <Route path="/parcours" element={<Parcours />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+    </>
   );
 }
 
