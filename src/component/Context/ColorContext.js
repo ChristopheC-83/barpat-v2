e44d50cbed2ba8d771529b7ac9e8e4f6b88c68e8
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-01-23 21:49:29 +0100

    test pour aujourd'hui ok, demain nav ?

--- src/component/Context/ColorContext.js
+++ src/component/Context/ColorContext.js
@@ -10,36 +10,53 @@ const DataColorContextProvider = (props) => {
       name: "cv",
       color: "rgb(16, 0, 160)",
       textCV: "Mon CV, tout ce qu'il y  a de plus classique",
+      to:"/",
+      nameLink:"Accueil",
+      bgc : "#333"
     },
     {
       id: 2,
       name: "informatique",
       color: "rgb(0, 0, 0)",
       text: "formations YT, Udemy et projets",
+      to:"/cv",
+      nameLink:"Mon CV",
+      bgc : "#333"
     },
     {
       id: 3,
       name: "parcours",
       color: "rgb(104, 0, 189)",
       text: "Mes études, exp pro et formations",
+      to:"/informatique",
+      nameLink:"Le Dev'",
+      bgc : "#333"
     },
     {
       id: 4,
       name: "partages",
       color: "rgb(136, 0, 0)",
       text: "à travers github, codepen & bientôt microtuto expliqués à un enfant de 12 ans.",
+      to:"/partages",
+      nameLink:"Mes partages",
+      bgc : "#333"
     },
     {
       id: 5,
       name: "portfolio",
       color: "#fff",
       text: "mes productions principales",
+      to:"/portfolio",
+      nameLink:"Portfolio",
+      bgc : "#333"
     },
     {
       id: 6,
       name: "sport",
       color: "rgb(25, 121, 16)",
-      text: "Ce que je fais, pourquoi, pour quoi, mes expériences notables.",
+      text: "Ce que je fais, pourquoi, pour quoi, mes expériences notables.", to:"/sport",
+      nameLink:"Un peu de Sport ?",
+      bgc : "#333"
     },
   ];
 
--- src/component/componentsNavbar/Navigation.js
+++ src/component/componentsNavbar/Navigation.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import {useContext} from 'react'
 import { NavLink } from "react-router-dom";
+import { dataColorContext } from '../Context/ColorContext';
 
 export default function Navigation() {
+
+  const { dataColor } = useContext(dataColorContext);
+  console.log("nav =>", dataColor[0].to)
+
   return (
     <div className="navigation">
         <ul>
           <li>
-            <NavLink to="/">
+            <NavLink to={dataColor[0].to}>
               <p>Accueil</p>
             </NavLink>
-            <NavLink to="/cv">
+            <NavLink to={dataColor[1].to}>
               <p>Mon CV</p>
             </NavLink>
             <NavLink to="/informatique">
