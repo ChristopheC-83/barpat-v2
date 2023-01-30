99eeae3f35a4e66564a98f340dfb18aaf7d4c276
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-01-30 17:04:38 +0100

    on attaque cv en TL

--- src/Pages/Partages.js
+++ src/Pages/Partages.js
@@ -1,4 +1,4 @@
-import Nav from '../component/Nav'
+import Nav from "../component/Nav";
 import { useContext } from "react";
 import { dataColorContext } from "../component/Context/ColorContext";
 
@@ -15,9 +15,17 @@ export default function Partages(props) {
           boxShadow: `0 0 80px 100px ${color}`,
         }}
       ></div>
-     <Nav color={color}/>
-    <div className='pageContainer partageContainer'>
-      partages
-    </div></div>
-  )
+      <Nav color={color} />
+      <div className="pageContainer partageContainer">
+        <div className="titlePage">
+          <h2>Bon, oui, ce n'est pas pour tout de suite...</h2>
+          <h2> Le but est de créer de petits tricks <br /> html/css pour commencer !</h2>
+          <h2> Puis de les partager sous forme d'une micro encyclopédie.</h2>
+          <h3>( + une notion en 2 minutes par jour pour apprendre comme à un enfant)</h3>
+          <br /><br /><br />
+          <h2>En attendant, <br /> je vais essayer de nourrir GitHub et CodePen...</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
