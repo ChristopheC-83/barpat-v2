7215d75b0d9834d3037a74ddfa492dce96192502
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-02-02 06:44:07 +0100

    soulignage navbar ok

--- src/component/componentsNavbar/Navigation.js
+++ src/component/componentsNavbar/Navigation.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { dataColorContext } from "../Context/ColorContext";
 
-export default function Navigation() {
+export default function Navigation({ active, children, onClick }) {
   const { dataColor } = useContext(dataColorContext);
 
   return (
@@ -10,7 +10,10 @@ export default function Navigation() {
       <ul>
         {dataColor.map((link) => (
           <li key={link.id}>
-            <NavLink to={link.to}>
+            <NavLink 
+            to={link.to} 
+            className={active ? "active liActive" : ""}
+            >
               <i className={link.icone} style={{ color: `${link.color}` }}></i>
 
               <p>{link.nameLink}</p>
